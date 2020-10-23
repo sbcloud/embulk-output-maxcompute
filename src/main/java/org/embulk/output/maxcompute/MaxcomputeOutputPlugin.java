0afbc69f4ceaead4a6ab14678577d544448cc2af
bwbw723 <bwq_q@hotmail.com>
2020-10-23 17:48:06 +0800

    [Bob] Init version of MaxCompute page plugin

--- src/main/java/org/embulk/output/maxcompute/MaxcomputeOutputPlugin.java
+++ src/main/java/org/embulk/output/maxcompute/MaxcomputeOutputPlugin.java
@@ -1,48 +1,172 @@
 package org.embulk.output.maxcompute;
 
+import com.aliyun.odps.Odps;
+import com.aliyun.odps.PartitionSpec;
+import com.aliyun.odps.account.Account;
+import com.aliyun.odps.account.AliyunAccount;
+import com.aliyun.odps.commons.util.RetryStrategy;
+import com.aliyun.odps.data.Record;
+import com.aliyun.odps.tunnel.TableTunnel;
+import com.aliyun.odps.tunnel.TunnelException;
+import com.aliyun.odps.tunnel.io.TunnelBufferedWriter;
+import com.google.common.base.Optional;
+import org.embulk.config.*;
+import org.embulk.spi.*;
+import org.embulk.spi.time.Timestamp;
+import org.embulk.spi.type.*;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
-import com.google.common.base.Optional;
-
-import org.embulk.config.Config;
-import org.embulk.config.ConfigDefault;
-import org.embulk.config.ConfigDiff;
-import org.embulk.config.ConfigSource;
-import org.embulk.config.Task;
-import org.embulk.config.TaskReport;
-import org.embulk.config.TaskSource;
-import org.embulk.spi.Exec;
-import org.embulk.spi.OutputPlugin;
-import org.embulk.spi.PageOutput;
-import org.embulk.spi.Schema;
-import org.embulk.spi.TransactionalPageOutput;
-
 public class MaxcomputeOutputPlugin
-        implements OutputPlugin
-{
+        implements OutputPlugin {
     public interface PluginTask
-            extends Task
-    {
-        // configuration option 1 (required integer)
-        @Config("option1")
-        public int getOption1();
+            extends Task {
+        @Config("accessKeyId")
+        public String getAccessKeyID();
 
-        // configuration option 2 (optional string, null is not allowed)
-        @Config("option2")
-        @ConfigDefault("\"myvalue\"")
-        public String getOption2();
+        @Config("accessKeySecret")
+        public String getAccessKeySecret();
 
-        // configuration option 3 (optional string, null is allowed)
-        @Config("option3")
+        @Config("odpsUrl")
+        @ConfigDefault("\"http://service.ap-northeast-1.maxcompute.aliyun.com/api\"")
+        public Optional<String> getOdpsUrl();
+
+        @Config("tunnelUrl")
+        @ConfigDefault("\"http://dt.ap-northeast-1.maxcompute.aliyun.com\"")
+        public Optional<String> getTunnelUrl();
+
+        @Config("projectName")
+        public String getProjectName();
+
+        @Config("tableName")
+        public String getTableName();
+
+        @Config("partition")
         @ConfigDefault("null")
-        public Optional<String> getOption3();
+        Optional<String> getPartition();
+    }
+
+    public class MaxcomputePageOutput implements TransactionalPageOutput {
+
+        private final Logger log = Exec.getLogger(MaxcomputePageOutput.class);
+
+        private final DateTimeFormatter TO_STRING_FORMATTER_MILLIS = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS ").withZoneUTC();
+        private PageReader pageReader;
+        private PluginTask task;
+        private Schema schema;
+        private TunnelBufferedWriter recordWriter;
+        private Odps odps;
+        private TableTunnel.UploadSession uploadSession;
+        private RetryStrategy retryStrategy;
+
+
+        public MaxcomputePageOutput(PluginTask task, Schema schema) {
+            this.pageReader = new PageReader(schema);
+            this.task = task;
+            this.schema = schema;
+            this.odps = generateOdpsClient(task);
+            this.uploadSession = generateTableUploadSession(odps, task);
+            this.retryStrategy = new RetryStrategy(6, 4, RetryStrategy.BackoffStrategy.EXPONENTIAL_BACKOFF);
+        }
+
+        private Odps generateOdpsClient(PluginTask task) {
+            if (task.getOdpsUrl().isPresent() && !task.getAccessKeyID().isEmpty() && !task.getAccessKeySecret().isEmpty() && !task.getProjectName().isEmpty()) {
+                Account account = new AliyunAccount(task.getAccessKeyID(), task.getAccessKeySecret());
+                Odps odps = new Odps(account);
+                odps.setEndpoint(task.getOdpsUrl().get());
+                odps.setDefaultProject(task.getProjectName());
+                return odps;
+            } else {
+                throw new UnsupportedOperationException("Less parameters for maxcompute output plugin to create ODPS client");
+            }
+        }
+
+        private TableTunnel.UploadSession generateTableUploadSession(Odps odps, PluginTask task) {
+            if (null != odps && task.getTunnelUrl().isPresent()) {
+                try {
+                    TableTunnel tableTunnel = new TableTunnel(odps);
+                    TableTunnel.UploadSession uploadSession;
+                    if (!task.getPartition().isPresent()) {
+                        log.info("Running with no partition mode");
+                        uploadSession = tableTunnel.createUploadSession(task.getProjectName(), task.getTableName());
+                    } else {
+                        log.info("Running with partition mode as : " + task.getPartition().get());
+                        PartitionSpec partitionSpec = new PartitionSpec(task.getPartition().get());
+                        uploadSession = tableTunnel.createUploadSession(task.getProjectName(), task.getTableName(), partitionSpec);
+                    }
+                    return uploadSession;
+                } catch (TunnelException e) {
+                    log.error(e.getErrorMsg());
+                    throw new UnsupportedOperationException("Failed to create table tunnel session");
+                }
+            } else {
+                throw new UnsupportedOperationException("Less parameters for maxcompute output plugin to create table tunnel session");
+            }
+        }
+
+        @Override
+        public void add(Page page) {
+            pageReader.setPage(page);
+
+            try {
+                recordWriter = (TunnelBufferedWriter) uploadSession.openBufferedWriter();
+                recordWriter.setRetryStrategy(retryStrategy);
+                Record tmp = uploadSession.newRecord();
+                while (pageReader.nextRecord()) {
+                    for (Column column : schema.getColumns()) {
+                        // Data Format https://github.com/alibaba/DataX/blob/master/odpswriter/doc/odpswriter.md
+                        if (column.getType() instanceof StringType) {
+                            tmp.setString(column.getName(), pageReader.getString(column));
+                        } else if (column.getType() instanceof BooleanType) {
+                            tmp.setBoolean(column.getName(), pageReader.getBoolean(column));
+                        } else if (column.getType() instanceof LongType) {
+                            tmp.setBigint(column.getName(), pageReader.getLong(column));
+                        } else if (column.getType() instanceof DoubleType) {
+                            tmp.setDouble(column.getName(), pageReader.getDouble(column));
+                        } else if (column.getType() instanceof TimestampType) {
+                            Timestamp timestamp = pageReader.getTimestamp(column);
+                            tmp.setDatetime(column.getName(), new Date(timestamp.toEpochMilli()));
+                        }
+                    }
+                    recordWriter.write(tmp);
+                }
+                recordWriter.close();
+                uploadSession.commit();
+            } catch (TunnelException | IOException e) {
+                throw new UnsupportedOperationException("Failed to upload related data");
+            }
+        }
+
+        @Override
+        public void finish() {
+
+        }
+
+        @Override
+        public void close() {
+
+        }
+
+        @Override
+        public void abort() {
+
+        }
+
+        @Override
+        public TaskReport commit() {
+            return Exec.newTaskReport();
+        }
     }
 
     @Override
     public ConfigDiff transaction(ConfigSource config,
-            Schema schema, int taskCount,
-            OutputPlugin.Control control)
-    {
+                                  Schema schema, int taskCount,
+                                  OutputPlugin.Control control) {
         PluginTask task = config.loadConfig(PluginTask.class);
 
         // retryable (idempotent) output:
@@ -55,25 +179,21 @@ public class MaxcomputeOutputPlugin
 
     @Override
     public ConfigDiff resume(TaskSource taskSource,
-            Schema schema, int taskCount,
-            OutputPlugin.Control control)
-    {
+                             Schema schema, int taskCount,
+                             OutputPlugin.Control control) {
         throw new UnsupportedOperationException("maxcompute output plugin does not support resuming");
     }
 
     @Override
     public void cleanup(TaskSource taskSource,
-            Schema schema, int taskCount,
-            List<TaskReport> successTaskReports)
-    {
+                        Schema schema, int taskCount,
+                        List<TaskReport> successTaskReports) {
     }
 
     @Override
-    public TransactionalPageOutput open(TaskSource taskSource, Schema schema, int taskIndex)
-    {
+    public TransactionalPageOutput open(TaskSource taskSource, Schema schema, int taskIndex) {
         PluginTask task = taskSource.loadTask(PluginTask.class);
-
-        // Write your code here :)
-        throw new UnsupportedOperationException("MaxcomputeOutputPlugin.run method is not implemented yet");
+        MaxcomputePageOutput pageOutput = new MaxcomputePageOutput(task, schema);
+        return pageOutput;
     }
 }
